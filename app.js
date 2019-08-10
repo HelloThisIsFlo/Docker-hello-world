6ebf6b8e7503b1a2bdb33cc5c3c2ff67f40ff6e5
Florian Kempenich <Flori@nKempenich.com>
2019-08-10 15:49:01 +0100

    Fix display & provide default value for 'world'

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express();
 const uniqueId = Math.ceil((Math.random() * 1000));
 const port = 80;
 
-const world = process.env.WORLD
+const world = process.env.WORLD || "world"
 
 app.get("/", function(req, res) {
     console.log(`Receiving request | uniqueId: ${uniqueId}`);
+    res.setHeader('Content-Type', 'text/plain')
     res.send(cowsay.say({
         text : `Hello ${world}, My uniqueId is ${uniqueId}`,
         f: 'whale'
